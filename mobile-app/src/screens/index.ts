75ef5c5750b0e88f6f7558babe6d69d0bbf3af3b
Kamwanga <kamwangaraheem2050@gmail.com>
2025-12-10 09:42:47 +0300

    Set up navigation structure

---
+++ mobile-app/src/screens/index.ts
@@ -0,0 +1,6 @@
+export { default as LoginScreen } from './LoginScreen';
+export { default as CountryListScreen } from './CountryListScreen';
+export { default as LocationSearchScreen } from './LocationSearchScreen';
+export { default as AdminLevelsScreen } from './AdminLevelsScreen';
+export { default as AdminUnitEditorScreen } from './AdminUnitEditorScreen';
+export { default as VersionHistoryScreen } from './VersionHistoryScreen';
